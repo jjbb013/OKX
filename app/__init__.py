6619798aa5c567119ea66844c36a692014725421
jjbb013 <59217852+jjbb013@users.noreply.github.com>
2025-06-26 00:15:35 +0800

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-# 确保strategies是一个Python包
-from pathlib import Path
-
-# 确保必要的目录存在
-PATH = Path(__file__).parent
-PATH.joinpath('templates').mkdir(exist_ok=True)
-PATH.joinpath('static').mkdir(exist_ok=True)
-PATH.joinpath('strategies').mkdir(exist_ok=True)
